acd346d82c25310ccee494880c6b3737f567a714
w3bgir1 <akulshyna@gmail.com>
2020-04-30 11:15:27 +0200

    init commit

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const AccountScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const TrackCreateScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackCreateScreen;
